da329c5d6849f1ffc6bfd4b2626765e786502d88
RamirezOlmos <nyy_715074@yahoo.com>
2023-05-23 09:48:09 -0600

    finish(random-quote-generator/src/App.tsx) finish the quote generator app. It could still be improve by creating a custom hook to manage the state of the Api to get the information

--- random-quote-generator/src/App.tsx
+++ random-quote-generator/src/App.tsx
@@ -1,11 +1,66 @@
-import classnames from "classnames"
-import { ReactComponent as Button } from "../src/assets/icons/button.svg"
-import { ReactComponent as Quotation } from "../src/assets/icons/quotation.svg"
-import { ReactComponent as Twitter } from "../src/assets/icons/twitter.svg"
-import { ReactComponent as Whatsapp } from "../src/assets/icons/whatsapp.svg"
-import "./App.css"
+import classnames from "classnames";
+import { useEffect, useState } from "react";
+import { ReactComponent as Button } from "../src/assets/icons/button.svg";
+import { ReactComponent as Quotation } from "../src/assets/icons/quotation.svg";
+import { ReactComponent as Twitter } from "../src/assets/icons/twitter.svg";
+import { ReactComponent as Whatsapp } from "../src/assets/icons/whatsapp.svg";
+import axios from "axios";
+import "./App.css";
+import { randomizeArray } from "./utils";
+
 
 function App() {
+  const [quoteList, setQuoteList] = useState<Quote[]>([]);
+  const [arrayCount, setArrayCount] = useState<number>(0);
+
+
+  const fetchData = async () => {
+    try {
+
+      const response = await axios.get('http://localhost:4000/quotes');
+      const data = randomizeArray(response.data);
+      setQuoteList(data);
+
+    } catch (error) {
+      console.log('Error:', error);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleNextPreviosQuote = (
+    arrayCount: number,
+    direction: string
+  ) => {
+    if (
+      direction === 'Next' && arrayCount < quoteList.length - 1
+    ) setArrayCount(arrayCount += 1);
+
+    if (
+      direction === 'Previous' && arrayCount > 0
+    ) setArrayCount(arrayCount -= 1);
+  }
+
+  if (arrayCount === 18) {
+    fetchData();
+    setArrayCount(0);
+  }
+
+  const shareOnWhatsApp = (quote: string, author: string) => {
+    const text = `${quote}\n- ${author}`;
+    const url = `https://api.whatsapp.com/send?text=${encodeURIComponent(text)}`;
+    window.open(url, '_blank');
+  }
+
+  const shareOnTwitter = (quote: string, author: string) => {
+    const text = `${quote}\n- ${author}`;
+    const shareUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(text)}`;
+    window.open(shareUrl, '_blank');
+  }
+
+
   return (
     <>
       <header>
@@ -16,20 +71,44 @@ function App() {
           <Quotation />
           <div className="quote">
             <p>
-              In the end, we will remember not the words of our enemies, but the
-              silence of our friends.
+              {
+                quoteList.length === 0 ? '' :
+                  quoteList[arrayCount].quote
+              }
             </p>
-            <span>- Martin Luther King Jr.</span>
+            <span>
+              {
+                quoteList.length === 0 ? '' :
+                  `- ${quoteList[arrayCount].author}`
+              }
+            </span>
           </div>
           <div className="bottom-navigation">
             <div>
-              <Button className={classnames("rotate cp")} />
-              <Button className="cp" />
+              <Button className={
+                arrayCount === 0 ? classnames("rotate cp disabled-button") :
+                  classnames("rotate cp")
+              }
+                onClick={() => handleNextPreviosQuote(arrayCount, "Previous")}
+              />
+              <Button className="cp"
+                onClick={() => handleNextPreviosQuote(arrayCount, "Next")}
+              />
             </div>
             <div className="share">
               <span>Share At:</span>
-              <Twitter title="Post this quote on twitter!" className="cp" />
-              <Whatsapp title="Post this quote on WhatsApp!" className="cp" />
+              <Twitter title="Post this quote on twitter!" className="cp"
+                onClick={() => shareOnTwitter(
+                  quoteList[arrayCount].quote,
+                  quoteList[arrayCount].author
+                )}
+              />
+              <Whatsapp title="Post this quote on WhatsApp!" className="cp"
+                onClick={() => shareOnWhatsApp(
+                  quoteList[arrayCount].quote,
+                  quoteList[arrayCount].author
+                )}
+              />
             </div>
           </div>
         </div>
